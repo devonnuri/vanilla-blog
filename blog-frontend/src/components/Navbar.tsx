c7fa3536483ecf63df6e40de1a22b98dce315aa2
뎁온누리 <devonnuri@gmail.com>
2018-07-10 22:47:28 +0900

    Remove Some Annoying Thingy and Start making Navbar

---
+++ blog-frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Devonnuri</h1>
+    </div>
+  );
+};
+
+const StyledNavbar = styled(Navbar)`
+  background-color: #f39c12;
+`;
+
+export default StyledNavbar;
