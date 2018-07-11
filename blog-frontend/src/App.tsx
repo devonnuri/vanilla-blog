4d5d325ff8c191244bb2b48978fb60f143c65224
뎁온누리 <devonnuri@gmail.com>
2018-07-11 20:49:22 +0900

    Header.. Maybe Done..?

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,20 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, Post, Profile } from './pages';
+import styled from 'styled-components';
 
 import Header from './components/Header';
+import { Home, Post } from './pages';
+
+const Container = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <Fragment>
+      <Container>
         <Header />
         <Route exact={true} path="/" component={Home} />
         <Switch>
-          <Route path="/:username/:postId" component={Post} />
-          <Route path="/:username" component={Profile} />
+          <Route path="/:postId" component={Post} />
         </Switch>
-      </Fragment>
+      </Container>
     );
   }
 }
--- blog-frontend/src/components/Header.tsx
+++ blog-frontend/src/components/Header.tsx
@@ -4,12 +4,37 @@ import styled from 'styled-components';
 
 const Header = styled.div`
   text-align: center;
+  margin: 3rem 0;
+`;
+
+const TitleContainer = styled.div`
+  padding: 0.3rem 0.3rem;
+  border: 2px solid black;
+
+  border-radius: 5px;
+
+  display: inline-block;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 1rem 2rem;
+
+  border: 2px solid black;
+  border-radius: 5px;
+
+  display: inline-block;
+
+  font-size: 5em;
+  font-weight: 300;
 `;
 
 export default () => {
   return (
     <Header>
-      <h1>뎁온누리</h1>
+      <TitleContainer>
+        <Title>뎁온누리</Title>
+      </TitleContainer>
     </Header>
   );
 };
