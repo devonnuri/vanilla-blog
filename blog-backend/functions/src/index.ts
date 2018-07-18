74494f8f897d6eb3ca000040d2747ce5ab81fb1d
뎁온누리 <devonnuri@gmail.com>
2018-07-18 22:34:32 +0900

    Start mongoose to backend

--- blog-backend/functions/src/index.ts
+++ blog-backend/functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import { existsSync, readFileSync } from 'fs';
+
+if (existsSync('.env')) {
+  mongoose.connect(
+    readFileSync('.env', 'utf8'),
+    {
+      useNewUrlParser: true,
+    }
+  );
+}
 
 const app = express();
 
-app.get('/get', (request, response) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/posts/get', (request, response) => {
   response.json([
     {
       id: 1,
@@ -12,10 +28,10 @@ app.get('/get', (request, response) => {
     },
     {
       id: 2,
-      title: 'Hello World!',
-      body: 'WoWOWOW',
+      title: 'The Second Ppost',
+      body: 'W0W0W0W0W',
     },
   ]);
 });
 
-export const post = functions.https.onRequest(app);
+export const api = functions.https.onRequest(app);
