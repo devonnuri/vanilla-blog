dfd1909fc2a534ba7f38b55677640c3e563f9b17
뎁온누리 <devonnuri@gmail.com>
2018-07-18 21:51:02 +0900

    Express Hello World!

--- blog-backend/functions/src/index.ts
+++ blog-backend/functions/src/index.ts
@@ -1,4 +1,21 @@
 import * as functions from 'firebase-functions';
-import express from 'express';
+import * as express from 'express';
 
 const app = express();
+
+app.get('/get', (request, response) => {
+  response.json([
+    {
+      id: 1,
+      title: 'Hello World!',
+      body: 'WoWOWOW',
+    },
+    {
+      id: 2,
+      title: 'Hello World!',
+      body: 'WoWOWOW',
+    },
+  ]);
+});
+
+export const post = functions.https.onRequest(app);
