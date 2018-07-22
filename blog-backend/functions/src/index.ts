4fdd6d3eebf14b2098bc864315c6d1d1751c8a01
뎁온누리 <devonnuri@gmail.com>
2018-07-22 10:16:46 +0900

    Authroization Done!

--- blog-backend/functions/src/index.ts
+++ blog-backend/functions/src/index.ts
@@ -1,20 +1,37 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
-
+import { initializeApp } from 'firebase-admin';
 import { config } from 'dotenv';
 
-import Posts from './routes/posts/';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+
+import { validateToken } from './lib/database';
+
+initializeApp();
+
+import Posts from './routes/posts';
 
 config();
 
-mongoose.connect(process.env.MONGODB).catch(err => {
-  console.error(err);
-});
-
 const app = express();
 
+app.use(
+  cors({
+    origin: true,
+  })
+);
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(validateToken);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
--- blog-backend/functions/src/routes/posts/posts.ctrl.ts
+++ blog-backend/functions/src/routes/posts/posts.ctrl.ts
@@ -1,25 +1,28 @@
 import { Request, Response } from 'express';
+import { firestore, initializeApp } from 'firebase-admin';
 import * as Joi from 'joi';
 
-import Post from '../../models/Post';
 import { validateSchema } from '../../lib/common';
 
+const postRef = firestore().collection('post');
+
 export const listPost = async (_, response: Response) => {
-  response.json(await Post.find());
+  postRef.get().then(snapshot => {
+    console.log(require('util').inspect(snapshot));
+  });
 };
 
 export const readPost = async (request: Request, response: Response) => {
-  const post = await Post.findOne({
-    id: request.params.postId,
-  });
-
-  if (post) {
-    response.json(post);
-  } else {
-    response.status(404).json({
-      name: 'POST_NOT_FOUND',
-    });
-  }
+  // const post = await Post.findOne({
+  //   id: request.params.postId,
+  // });
+  // if (post) {
+  //   response.json(post);
+  // } else {
+  //   response.status(404).json({
+  //     name: 'POST_NOT_FOUND',
+  //   });
+  // }
 };
 
 export const writePost = async (request: Request, response: Response) => {
@@ -37,14 +40,14 @@ export const writePost = async (request: Request, response: Response) => {
     return;
   }
 
-  const { title, body } = request.body;
+  // const { title, body } = request.body;
 
-  const post = new Post({
-    id: Post.getLastPostId() + 1,
-    title,
-    body,
-    createdAt: new Date(),
-  });
-  post.save();
-  response.json(post);
+  // const post = new Post({
+  //   id: Post.getLastPostId() + 1,
+  //   title,
+  //   body,
+  //   createdAt: new Date(),
+  // });
+  // post.save();
+  // response.json(post);
 };
