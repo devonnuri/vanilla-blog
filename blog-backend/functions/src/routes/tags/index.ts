ac39d2922af7e3f6badf61a268acc60ec8539593
devonnuri <devonnuri@gmail.com>
2018-09-02 12:25:52 +0900

    TagManager Done!

--- blog-backend/functions/src/routes/tags/index.ts
+++ blog-backend/functions/src/routes/tags/index.ts
@@ -6,7 +6,7 @@ import authToken from '../../lib/middleware/authToken';
 const Tags = Router();
 
 Tags.get('/', postsCtrl.listTag);
-Tags.post('/create', authToken, postsCtrl.createTag);
+Tags.post('/create/:tagName', authToken, postsCtrl.createTag);
 Tags.post('/delete/:tagName', authToken, postsCtrl.deleteTag);
 
 export default Tags;
--- blog-backend/functions/src/routes/tags/tags.ctrl.ts
+++ blog-backend/functions/src/routes/tags/tags.ctrl.ts
@@ -1,26 +1,26 @@
 import { Request, Response } from '../../lib/express';
 import { firestore } from 'firebase-admin';
-import * as Joi from 'joi';
-import { validateSchema } from '../../lib/common';
 
 const tagsRef = firestore().collection('tags');
 
 export const createTag = async (request: Request, response: Response) => {
-  const schema = Joi.object().keys({
-    name: Joi.string()
-      .required()
-      .min(1)
-      .max(50),
-  });
+  const { tagName: name } = request.params;
 
-  if (!validateSchema(request, response, schema)) {
-    return;
-  }
+  tagsRef
+    .where('name', '==', name)
+    .get()
+    .then(snapshot => {
+      if (!snapshot.empty) {
+        response.status(409).json({
+          name: 'DUPLICATED_TAG',
+          payload: name,
+        });
+        return;
+      }
 
-  const { name } = request.body;
-
-  tagsRef.add({ name });
-  response.json({ name });
+      tagsRef.add({ name });
+      response.json({ name });
+    });
 };
 
 export const listTag = async (request: Request, response: Response) => {
